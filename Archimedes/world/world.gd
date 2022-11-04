e91845328f7537f5ad47acd8ae8342839ef46077
AthertonRoamer <111521556+AthertonRoamer@users.noreply.github.com>
2022-11-04 17:46:29 -0500

    added sword
    -added sword image
    -updated gitignore
    -updated menu (very little)

--- Archimedes/world/world.gd
+++ Archimedes/world/world.gd
@@ -3,11 +3,20 @@ onready var bkgd = $background
 onready var b_offset = Vector2(557, 405)
 onready var note = $death_note
 var n_offest = Vector2(-174, -104)
+onready var butn = $menu
+var m_offset = Vector2(-103, 35)
 
 func _ready():
-	pass 
+	get_tree().paused = false
+	butn.pause_mode = Node.PAUSE_MODE_PROCESS
 
 func _process(_delta):
 	bkgd.set_position(get_node("player/Camera2D").get_camera_screen_center() - b_offset)
 	note.set_position(get_node("player/Camera2D").get_camera_screen_center() + n_offest)
+	butn.set_position(get_node("player/Camera2D").get_camera_screen_center() + m_offset)
 	#moves background and label to correct positions relative to camera
+
+
+func _on_menu_pressed():
+	get_tree().change_scene("res://menu.tscn")
+	
