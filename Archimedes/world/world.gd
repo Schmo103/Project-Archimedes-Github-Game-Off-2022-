3b7188c30a22fd4ef4287eb36f0dd8212a647c85
Schmo103 <112767599+Schmo103@users.noreply.github.com>
2022-11-08 16:28:00 -0600

    worked on firesprite motion and messed with gameplay(lava speed)

--- Archimedes/world/world.gd
+++ Archimedes/world/world.gd
@@ -8,8 +8,8 @@ onready var fire_sprite = load('res://firesprite.tscn')
 var spawnable = true
 var screen_x = OS.get_window_size().x
 var screen_y = OS.get_window_size().y
-var rand_x = randi() % screen_x
-var rand_y = randi() % screen_y
+onready var rand_x
+onready var rand_y 
 
 onready var bkgd = $background
 onready var b_offset = Vector2(557, 405)
@@ -21,6 +21,7 @@ var m_offset = Vector2(-103, 35)
 func spawner():
 	if spawnable == true:
 		timer.start(spawn_time)
+		spawnable = false
 	else:
 		timer.stop()
 	
@@ -29,6 +30,9 @@ func spawner():
 func _on_fire_sprite_timer_timeout():
 	var instance = fire_sprite.instance()
 	add_child(instance)
+	rand_x = randi() % int(screen_x)
+#	rand_y = randi() % int(screen_y)
+	rand_y = $Lava.HEIGHT + 100
 	instance.position = Vector2(rand_x,rand_y)
 
 
@@ -36,7 +40,7 @@ func _on_fire_sprite_timer_timeout():
 func _ready():
 	get_tree().paused = false
 	butn.pause_mode = Node.PAUSE_MODE_PROCESS
-  spawner()
+	spawner()
 	print(screen_x)
 	print(screen_y)
 
@@ -45,6 +49,7 @@ func _process(_delta):
 	note.set_position(get_node("player/Camera2D").get_camera_screen_center() + n_offest)
 	butn.set_position(get_node("player/Camera2D").get_camera_screen_center() + m_offset)
 	#moves background and label to correct positions relative to camera
+	
 
 
 func _on_menu_pressed():
