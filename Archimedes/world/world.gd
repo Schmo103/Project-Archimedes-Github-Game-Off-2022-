2af5523fab15f618048c0c8b7b62bab85e659888
Schmo103 <112767599+Schmo103@users.noreply.github.com>
2022-11-12 10:12:10 -0600

    minor changes with explosions

--- Archimedes/world/world.gd
+++ Archimedes/world/world.gd
@@ -5,7 +5,7 @@ var lava = "/root/World/Lava"
 
 
 onready var timer = $fire_sprite_timer
-onready var spawn_time = 1
+onready var spawn_time = 7
 onready var fire_sprite = load('res://firesprite.tscn')
 var spawnable = true
 var screen_x = OS.get_window_size().x
@@ -13,6 +13,8 @@ var screen_y = OS.get_window_size().y
 
 onready var rand_x
 onready var rand_y 
+
+signal firesprite_hits_player 
 
 
 onready var bkgd = $background
@@ -24,12 +26,13 @@ var m_offset = Vector2(-103, 35)
 
 func spawner():
 	if spawnable == true:
-		timer.start(spawn_time)
+		timer.start(randi() % spawn_time + 1)
 		spawnable = false
 	else:
 		timer.stop()
 	
-
+func firesprite_ex():
+	emit_signal("firesprite_hits_player")
 
 func _on_fire_sprite_timer_timeout():
 	var instance = fire_sprite.instance()
@@ -38,6 +41,7 @@ func _on_fire_sprite_timer_timeout():
 #	rand_y = randi() % int(screen_y)
 	rand_y = $Lava.HEIGHT + 100
 	instance.position = Vector2(rand_x,rand_y)
+	timer.wait_time = randi() % spawn_time + 1
 
 
 
@@ -53,6 +57,7 @@ func _process(_delta):
 	note.set_position(get_node("player/Camera2D").get_camera_screen_center() + n_offest)
 	butn.set_position(get_node("player/Camera2D").get_camera_screen_center() + m_offset)
 	#moves background and label to correct positions relative to camera
+	
 	
 
 
